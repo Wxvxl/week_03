8310f0cc2077ca9698ea98ab698eebb437f6759e
Wxvxl <metal123cool1115@gmail.com>
2022-04-01 11:22:10 +0800

    Refactor broken_score.py to implement functions.

--- broken_score.py
+++ broken_score.py
@@ -3,12 +3,29 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid Score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def get_score():
+    score = float(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid Score")
+        score = float(input("Enter score: "))
+    return score
+
+
+def check_grades(score):
+    if score < 0 or score > 100:
+        grade = "Invalid Score"
+    elif score >= 90:
+        grade = "Excellent"
+    elif score >= 50:
+        grade = "Passable"
+    else:
+        grade = "Bad"
+    return grade
+
+
+def main():
+    score = get_score()
+    grade = check_grades(score)
+    print(grade)
+
